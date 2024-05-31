a15eae866ba2491ffeca2e1fbed60a1faea62c33
Jacob Zhu <jacobz0106@gmail.com>
2024-05-31 13:48:44 -0700

    upd time

--- batchTask/sbatch.sh
+++ batchTask/sbatch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err bruss.sh 
-sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err randbruss.sh 
+sbatch --time=90:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err bruss.sh 
+sbatch --time=90:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err randbruss.sh 
