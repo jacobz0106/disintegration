89a60132021d19e15c7b2add695f46fabf8b2747
Jacob Zhu <jacobz0106@gmail.com>
2025-03-07 14:26:26 -0800

    upd

--- batchTask/sbatch.sh
+++ batchTask/sbatch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sbatch --time=82:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err bruss.sh 
-sbatch --time=82:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err randbruss.sh 
+sbatch --time=120:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/Naive.out -e out/Naive.err Naive.sh 
+sbatch --time=120:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/Rand.out -e out/Rand.err ML_Rand.sh
+sbatch --time=120:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/POF.out -e out/POF.err POF.sh
