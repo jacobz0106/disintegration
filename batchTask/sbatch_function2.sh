8df132d1bac25666097011561cc9d1644c11f7e7
Jacob Zhu <jacobz0106@gmail.com>
2025-05-26 15:42:06 -0700

    function2

---
+++ batchTask/sbatch_function2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#!/bin/bash
+models=("PPSVMG" "NN")
+samples=(20 10 5)
+methods=("Random" "POF")
+
+for model in "${models[@]}"; do
+  for size in "${samples[@]}"; do
+    for method in "${methods[@]}"; do
+      jobname="function2_${model}_${size}_${method}"
+      sbatch --time=13-00:00:00 \
+             --cpus-per-task=5 \
+             --mem-per-cpu=5G \
+             -o out/${jobname}.out \
+             -e out/${jobname}.err \
+             ${jobname}.sh
+    done
+  done
+done
