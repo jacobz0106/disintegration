34de03c01f7f6da7e5578119b83feadf273878fa
Jacob Zhu <jacobz0106@gmail.com>
2025-05-26 18:55:41 -0700

    upd

--- batchData/sbatch.sh
+++ batchData/sbatch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #!/bin/bash
-exampless=("lotka")
+examples=("lotka")
 samples=(20 10 5)
 methods=("Random" "POF")
 
 for example in "${examples[@]}"; do
   for size in "${samples[@]}"; do
     for method in "${methods[@]}"; do
-      jobname="brusselator_${example}_${size}_${method}"
+      jobname="${example}_${size}_${method}"
       sbatch --time=13-00:00:00 \
              --cpus-per-task=5 \
              --mem-per-cpu=5G \
