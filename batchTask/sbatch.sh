de44ea483d1e9bea67b73be05cc87ba69a97d3d0
Jacob Zhu <jacobz0106@gmail.com>
2025-06-05 14:01:25 -0700

    lotka

--- batchTask/sbatch.sh
+++ batchTask/sbatch.sh
@@ -7,7 +7,7 @@ methods=("Random" "POF")
 for model in "${models[@]}"; do
   for size in "${samples[@]}"; do
     for method in "${methods[@]}"; do
-      jobname="brusselator_${model}_${size}_${method}"
+      jobname="lotka_${model}_${size}_${method}"
       sbatch --time=13-00:00:00 \
              --cpus-per-task=5 \
              --mem-per-cpu=5G \
