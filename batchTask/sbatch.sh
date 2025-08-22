c55483328150b34375468c7a83073adb685867d7
Jacob Zhu <jacobz0106@gmail.com>
2025-08-22 12:40:01 -0700

    upd 5G - 10G mem

--- batchTask/sbatch.sh
+++ batchTask/sbatch.sh
@@ -10,7 +10,7 @@ for model in "${models[@]}"; do
       jobname="lotka_${model}_${size}_${method}"
       sbatch --time=13-00:00:00 \
              --cpus-per-task=5 \
-             --mem-per-cpu=5G \
+             --mem-per-cpu=10G \
              -o out/${jobname}.out \
              -e out/${jobname}.err \
              ${jobname}.sh
