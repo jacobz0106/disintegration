cb36424029538c8cc9140369bf882ee08f05497d
Jacob Zhu <jacobz0106@gmail.com>
2026-09-02 15:29:36 -0700

    Retarget sbatch.sh to brusselator NN sweep
    Trimmed walltime from 6d23h to 2d and mem from 20G to 5G/cpu since the NN runs don't need the PPSVMG-sized budget.

--- batchTask/sbatch.sh
+++ batchTask/sbatch.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#!/bin/bash
-models=("PPSVMG" "NN")
+models=("NN")
 samples=(20 10 5)
 methods=("Random" "POF")
 
 for model in "${models[@]}"; do
   for size in "${samples[@]}"; do
     for method in "${methods[@]}"; do
-      jobname="lotka_${model}_${size}_${method}"
-      sbatch --time=6-23:00:00 \
-             --cpus-per-task=8 \
-             --mem-per-cpu=20G \
+      jobname="brusselator_${model}_${size}_${method}"
+      sbatch --time=2-00:00:00 \
+             --cpus-per-task=5 \
+             --mem-per-cpu=5G \
              -o out/${jobname}.out \
              -e out/${jobname}.err \
              ${jobname}.sh
